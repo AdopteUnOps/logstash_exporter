621460ab6e1ac5b7549466e1e6f110a781c4e6f9
jetoile <kmx.petals@gmail.com>
2017-06-19 18:14:46 +0200

    fix typo

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-/go/bin/logstash_exporter -exporter.bind_address :${LOGSTASH_BIND_ADDRESS} -logstash.endpoint ${LOGSTASG_ENDPOINT}
+/go/bin/logstash_exporter -exporter.bind_address :${LOGSTASH_BIND_ADDRESS} -logstash.endpoint ${LOGSTASH_ENDPOINT}
